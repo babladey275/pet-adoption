9371be6a8a53ad24bba22717e773c044df604018
babladey275 <babladey275@gmail.com>
2024-10-07 02:01:23 +0600

    dynamic categories button and all pets display promises done

---
+++ js/video.js
@@ -0,0 +1,59 @@
+
+// categories
+const loadCategories = async() => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/peddy/categories`);
+    const data = await response.json();
+    displayCategories(data.categories);
+}
+
+// all pets
+
+const loadAllPets = async() => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`);
+    const data = await res.json();
+    displayPets(data.pets);
+
+}
+
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories");
+    categories.forEach(item => {
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML = `
+        <button class="flex justify-center items-center font-bold px-10 py-3 border gap-3"><img class="w-10 h-10" src="${item.category_icon}"> ${item.category}</button>
+        `;
+        categoryContainer.append(buttonContainer);
+    });
+}
+
+const displayPets = (pets) => {
+    const petContainer = document.getElementById("pets");
+
+    pets.forEach(pet => {
+        const card = document.createElement("div");
+        card.classList = "card border"
+        card.innerHTML = `
+        <figure class="px-5 pt-5">
+        <img
+          src="${pet.image}"
+          alt=""
+          class="rounded-xl h-full w-full object-cover"
+        />
+      </figure>
+      <div class="p-5">
+        <h2 class="font-bold font-inter text-xl">${pet.pet_name}</h2>
+        <div class="font-lato text-gray-500">
+        <p><i class="fa-solid fa-shield-cat"></i> Breed: ${pet.breed ? pet.breed : "Not available"}</p>
+        <p><i class="fa-regular fa-calendar"></i> Birth: ${pet.date_of_birth ? pet.date_of_birth : "Not available"}</p>
+        <p><i class="fa-solid fa-mercury"></i> Gender: ${pet.gender ? pet.gender : "Not available"}</p>
+        <p>$ Price: ${pet.price ? pet.price : "Not available"}</p>
+        </div>
+      </div>
+        `;
+        petContainer.append(card);
+    });
+}
+
+
+loadCategories();
+loadAllPets();
